fc80441f27177a308b6c2a51431b6a1e57ccbd05
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-07-23 10:20:06 +0930

    Small textual updates.

--- src/controllers/feeds.ts
+++ src/controllers/feeds.ts
@@ -8,7 +8,10 @@ export const getPosts = (
   // Http status code 200 is used to tell the client that the GET request is sucessful
   res.status(200).json({
     posts: [
-      { title: 'this is the first post', content: 'this is the first post' }
+      {
+        title: 'This is the first post',
+        content: 'This is the content of first post'
+      }
     ]
   });
 };
